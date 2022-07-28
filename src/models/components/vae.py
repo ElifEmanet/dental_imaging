5b1e1b1c0854a74ebe097e62a8dc1dea19a83871
Elif Emanet <emanete@student.ethz.ch>
2022-07-28 10:40:55 +0200

    vae working now

--- src/models/components/vae.py
+++ src/models/components/vae.py
@@ -21,25 +21,23 @@ class Encoder(nn.Module):
             nn.BatchNorm2d(16),
             nn.ReLU(True),
             nn.Conv2d(16, 32, 3, stride=stride, padding=0),
-            nn.ReLU(True),
-            nn.Conv2d(32, 64, 3, stride=stride, padding=0),
             nn.ReLU(True)
         )
 
         ### Flatten layer
         self.flatten = nn.Flatten(start_dim=1)
         ### Linear section
-        input_dim = int(((input_pxl / stride) / stride) / stride / stride)
+        input_dim = int(((input_pxl / stride) / stride) / stride)
 
         self.encoder_lin = nn.Sequential(
             nn.Linear(input_dim * input_dim * 32, fc2_input_dim),
             nn.ReLU(True),
             nn.Linear(fc2_input_dim, encoded_space_dim)
         )
-        ### mu: Mean of the latent Gaussian
+        ### mu
         self.hidden2mu = nn.Linear(encoded_space_dim, encoded_space_dim)
 
-        ### log_var: Standard deviation of the latent Gaussian
+        ###
         self.hidden2log_var = nn.Linear(encoded_space_dim, encoded_space_dim)
 
     def forward(self, x):
@@ -51,7 +49,7 @@ class Encoder(nn.Module):
         log_var = self.hidden2log_var(latent)
         return mu, log_var
 
-
+# decoder for 28 x 28 images
 class Decoder(nn.Module):
 
     def __init__(
@@ -62,7 +60,7 @@ class Decoder(nn.Module):
             input_pxl: int = 28
     ):
         super().__init__()
-        input_dim = int(((input_pxl / stride) / stride) / stride / stride)
+        input_dim = int(((input_pxl / stride) / stride) / stride)
         self.decoder_lin = nn.Sequential(
             nn.Linear(encoded_space_dim, fc2_input_dim),
             nn.ReLU(True),
@@ -73,8 +71,6 @@ class Decoder(nn.Module):
         self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, input_dim, input_dim))
 
         self.decoder_conv = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, 3, stride=stride, output_padding=0),
-            nn.ReLU(True),
             nn.ConvTranspose2d(32, 16, 3, stride=stride, output_padding=0),
             nn.BatchNorm2d(16),
             nn.ReLU(True),
